260aebf3f9bcc2440048e9c3bee016e5a839ce98
Seba <sebsym96@gmail.com>
2026-06-26 06:55:57 +0200

    feat: add Google Tag Analytics (G-KSRW79Y4C6)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,10 @@ export default defineNuxtConfig({
           href: 'https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;1,300;1,400&family=Raleway:wght@300;400;500;600&display=swap',
         },
       ],
+      script: [
+        { src: 'https://www.googletagmanager.com/gtag/js?id=G-KSRW79Y4C6', async: true },
+        { innerHTML: "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-KSRW79Y4C6');" },
+      ],
     },
   },
 })
